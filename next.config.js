ee2043ea7a0f4d23db0b67d7d98587e0a0aed9d0
unknown <sirine.JNAYEH@esprit.tn>
2023-09-25 11:54:56 +0100

    fixed communities/[id]

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript:{
+  /*typescript:{
     ignoreBuildErrors:true,
-  },
+  },*/
   experimental: {
     serverActions: true,
     serverComponentsExternalPackages: ["mongoose"],
